9e5664e4dc43f2578cdb28aed9306cd438a796f0
Aidil <aidilrozi83@gmail.com>
2020-07-31 04:35:34 -0500

    All images are displayed in Row component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import requests from './requests';
 function App() {
 	return (
 		<div className='App'>
-			<h1>Netflix-clone</h1>
+			<h1>Netflix-Clone</h1>
 			<Row
 				title='NETFLIX ORIGINALS'
 				fetchUrl={requests.fetchNetflixOriginals}
 			/>
 			<Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+			<Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+			<Row title='Action Movie' fetchUrl={requests.fetchActionMovies} />
+			<Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+			<Row title='Horrow Movies' fetchUrl={requests.fetchHorrorMovies} />
+			<Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+			<Row title='Documentaries' fetchUrl={requests.fetchDocoumentMovies} />
 		</div>
 	);
 }
